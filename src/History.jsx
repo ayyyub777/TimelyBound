5eee3cd0e73b7d8e9aa119ba4d5b9eb773e0e667
ayo <ayo@pop-os.localdomain>
2023-09-25 23:58:33 +0100

    Add TailwindCSS

--- src/History.jsx
+++ src/History.jsx
@@ -2,28 +2,28 @@ import { useContext } from "react";
 import { TimerContext } from "./contexts/TimerContext";
 
 export default function History() {
-  const {storageData} = useContext(TimerContext);
+  const { storageData } = useContext(TimerContext);
   return (
-     <table>
-        <thead>
-          <tr>
+    <table>
+      <thead>
+        <tr>
           <th>Task</th>
           <th>Duration</th>
           <th>Creation Date</th>
         </tr>
-        </thead>
-        <tbody>
-          {storageData.map((item) => {
-            const {activity, duration} = item;
-            return (
-              <tr key={`${activity}${Math.random()}`}>
-                <td>{activity}</td>
-                <td>{duration}</td>
-                <td>2 Days ago</td>
-              </tr>
-            )
-          })}
-        </tbody>
-   </table> 
+      </thead>
+      <tbody>
+        {storageData.map((item) => {
+          const { activity, duration } = item;
+          return (
+            <tr key={`${activity}${Math.random()}`}>
+              <td>{activity}</td>
+              <td>{duration}</td>
+              <td>2 Days ago</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
