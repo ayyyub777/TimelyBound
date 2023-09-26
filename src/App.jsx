7a2f238431b0e21a4d6dc89041276eca897f7fff
ayo <ayo@pop-os.localdomain>
2023-09-26 19:12:04 +0100

    Style NavBar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { TimerProvider } from './contexts/TimerContext';
-import NavBar from './components/NavBar';
-import PageNotFound from './components/PageNotFound';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { TimerProvider } from "./contexts/TimerContext";
+import NavBar from "./components/NavBar";
+import PageNotFound from "./components/PageNotFound";
 import History from "./History";
 import Timer from "./Timer";
 
@@ -11,13 +11,13 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/" element={<Timer/>}/>
-          <Route path="/history"element={<History/>}/>
+          <Route path="/" element={<Timer />} />
+          <Route path="/history" element={<History />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Router>
     </TimerProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      gray: {
+        white: "#FFFFFF",
+        title: "#E1E1E6",
+        text: "#C4C4CC",
+        label: "#8D8D99",
+        placeholder: "#7C7C8A",
+        divider: "#323238",
+        element: "#202024",
+        background: "#121214",
+      },
+      brand: {
+        light: "#00B37E",
+        principal: "#00875F",
+        dark: "#015F43",
+      },
+      error: {
+        text: "#F03847",
+        assets: "#CC2937",
+      },
+    },
     extend: {},
   },
   plugins: [],
